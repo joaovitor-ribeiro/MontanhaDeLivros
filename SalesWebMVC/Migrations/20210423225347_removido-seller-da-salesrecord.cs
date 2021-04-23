63d3641a38fe2e883f00a7fa8c3af59959941a32
Joao Pedro Alves <joaopa644@gmail.com>
2021-04-23 20:40:05 -0300

    adicionado combobox ao livro

---
+++ SalesWebMVC/Migrations/20210423225347_removido-seller-da-salesrecord.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MontanhaDeLivros.Migrations
+{
+    public partial class removidosellerdasalesrecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "SalesRecord",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "SalesRecord");
+        }
+    }
+}
